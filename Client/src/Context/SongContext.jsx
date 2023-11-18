50c2bb97d52d4c512a23505c6ebdba23613308df
Ayush Mhetre <mhetreayush1719@gmail.com>
2023-11-18 12:41:21 +0530

    Sidebar and Song Context

---
+++ Client/src/Context/SongContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useRef } from "react";
+
+export const SongContext = createContext();
+
+export const SongContextState = ({ children }) => {
+  let __URL__ = "http://localhost:1337";
+  const audio = new Audio();
+  const song = {
+    songUrl: "",
+    songName: "",
+    songArtist: "",
+    songAlbum: "",
+    isPlaying: false,
+
+    setSongUrl: (url) => {
+      song.songUrl = url;
+    },
+    setSongName: (name) => {
+      song.songName = name;
+    },
+    setArtistName: (name) => {
+      song.songArtist = name;
+    },
+    setAlbumName: (name) => song.songAlbum = name,
+    setIsPlaying : ( val )=>{
+      song.isPlaying = val
+    },
+    
+  };
+
+  return <SongContext.Provider value={{audio,song,__URL__}}>{children}</SongContext.Provider>;
+};
